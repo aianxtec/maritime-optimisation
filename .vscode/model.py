c085033a430677a9f7d86524748deb41c024ed61
Reza Abusaidi <reza.abusaidi@aianxtec.com>
2021-12-18 16:26:09 +0000

    data collection setup completed

--- .vscode/model.py
+++ .vscode/model.py
@@ -1,22 +1,39 @@
+from matplotlib.colors import Colormap
+from seaborn.utils import ci
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import power_transform
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+import matplotlib
 import pandas as pd
+import numpy as np
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
-file = 'voyage-data.csv'
+filevessel = 'vesseldata.csv'
+fileenv = 'envdata.csv'
+filefuel = 'fueldata.csv'
 
 voyage = pd.read_csv(file)
 
 
 Xvar = voyage.drop(columns=['SOX', 'NOX'], axis=1)
-yVar = (voyage[['SOX']])
+yVar = (voyage[['SOX', 'NOX']])
 
+
+
+
+# sns.regplot(x=Xvar['Engine-RPM'], y=yVar['SOX'], data=voyage)
+
+Xvar.loc[:, 'DFT'] = 12.1 #from AIS data
+
+# plt.show()
 print(Xvar, yVar)
-
-
-sns.regplot(x=Xvar, y=yVar, data=voyage)
-plt.show()
-
-
-
 
 # print(voyage.info())
---
+++ Model.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+filename = 'voyage-data.csv'
+
+df = pd.read_csv(filename)
