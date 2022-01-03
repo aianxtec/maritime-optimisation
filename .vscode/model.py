5f884b597caf5144172953e97554f717fd0037c4
Reza Abusaidi <reza.abusaidi@aianxtec.com>
2022-01-03 15:24:01 +0000

    power-transform and Kde

--- .vscode/model.py
+++ .vscode/model.py
@@ -2,6 +2,8 @@ from matplotlib.colors import Colormap
 from seaborn.utils import ci
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import power_transform
+from sklearn.preprocessing import PowerTransformer
+
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
@@ -15,6 +17,8 @@ warnings.filterwarnings("ignore")
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from sklearn.preprocessing import MinMaxScaler
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.preprocessing import StandardScaler
 
 filevessel = 'vesseldata.csv'
 fileenv = 'envdata.csv'
@@ -35,6 +39,12 @@ envdf["Time"] = pd.to_datetime(envdf["Time"])
 
 # print(f"{vesseldf.info()}\n {envdf.info()}\n{fueldf.info()}")
 
+
+
+
+###################################
+# D  A  T  A  ***  A  C  Q  U  I  S  I  T  I  O  N
+
 CombinedMetrics = pd.DataFrame()
 CombinedMetrics = pd.concat([vesseldf, envdf, fueldf], axis=1) #joined all datasources into one DF for easier manipulation
 
@@ -52,6 +62,15 @@ Xvar = CombinedMetrics.drop(columns=['SOX', 'NOX', 'Date', 'Time', 'Viscosity_cs
 # print(Xvar, yVar)
 
 XandY = pd.concat([Xvar, yVar], axis=1)
+
+
+
+
+
+
+
+#######################################
+# M  I  N - M  A  X   ***  S  C  A  L  A  R
 
 # define min max scaler
 scaler = MinMaxScaler()
@@ -79,8 +98,18 @@ Xfeat= normData.iloc[:,0:13]
 ytarget = normData.iloc[:,-1]
 
 
-# X_train, X_test, y_train, y_test= train_test_split(
-#     Xfeat, ytarget, test_size=0.3, random_state=1)
+
+
+
+X_train, X_test, y_train, y_test= train_test_split(
+    Xfeat, ytarget, test_size=0.33, random_state=101)
+
+#standardization scaler - fit&transform on train, fit only on test
+
+s_scaler = StandardScaler()
+X_train = s_scaler.fit_transform(X_train.astype(np.float))
+X_test = s_scaler.transform(X_test.astype(np.float))
+
 
 
 
@@ -89,41 +118,9 @@ ytarget = normData.iloc[:,-1]
 rf_model= RandomForestClassifier( max_depth=12, n_estimators = 300)
 
 
-rf_model.fit(Xfeat.astype(int), ytarget.astype(int))
+rf_model.fit(X_train.astype(int), y_train.astype(int))
 
 
-
-# train_features = X_train.columns
-# importances = rf_model.feature_importances_
-# indices = np.argsort(importances)[-8:]  # top features
-# plt.title("Feature Importance")
-# plt.barh(range(len(indices)),
-#          importances[indices], color='g', align='center')
-# plt.yticks(range(len(indices)), [
-#            train_features[i] for i in indices])
-# plt.xlabel('Relative Importance')
-# # plt.xlim(0, 0.6)
-# plt.tight_layout()
-# plt.show()
-
-
-
-# use feature importance for feature selection
-
-from sklearn.inspection import permutation_importance
-from matplotlib import pyplot as plt
-
-
-# from xgboost import XGBRegressor
-
-X_train, X_test, y_train, y_test= train_test_split(
-    Xfeat, ytarget, test_size=0.33, random_state=101)
-
-#standardization scaler - fit&transform on train, fit only on test
-from sklearn.preprocessing import StandardScaler
-s_scaler = StandardScaler()
-X_train = s_scaler.fit_transform(X_train.astype(np.float))
-X_test = s_scaler.transform(X_test.astype(np.float))
 
 
 # Multiple Liner Regression
@@ -139,7 +136,7 @@ y_pred = regressor.predict(X_test)
 coeff_df = pd.DataFrame(regressor.coef_, normData.drop(['Viscosity_cst', 'NOX', 'SOX'],axis =1).columns, columns=['Coefficient']) 
 print(coeff_df)
 
-# visualizing residuals
+# visualizing residuals (distance between predictions and actual through regression line)
 fig = plt.figure(figsize=(10,5))
 residuals = (y_test- y_pred)
 sns.distplot(residuals)
@@ -177,69 +174,68 @@ plt.show()
 
 
 
-
-
-# ###############################################################################
-# F    E    A    T    U    R    E   •••••  S    E    L   L   E   C   T   I   O   N
+# # ###############################################################################
+# # F    E    A    T    U    R    E   •••••  S    E    L   L   E   C   T   I   O   N
 
   
-# plt.barh(boston.feature_names, xgb.feature_importances_)
+# # plt.barh(boston.feature_names, xgb.feature_importances_)
 
-X = normData.iloc[:,0:13].astype(int)  #independent columns
-y = normData.iloc[:,-1].astype(int)   #viscosity
-from sklearn.ensemble import ExtraTreesClassifier
-import matplotlib.pyplot as plt
-model = ExtraTreesClassifier()
-model.fit(X,y)
-print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+# X = normData.iloc[:,0:13].astype(int)  #independent columns
+# y = normData.iloc[:,-1].astype(int)   #viscosity
+# from sklearn.ensemble import ExtraTreesClassifier
+# import matplotlib.pyplot as plt
+# model = ExtraTreesClassifier()
+# model.fit(X,y)
+# print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
 
-feat_importances = pd.Series(model.feature_importances_, index=X.columns)
-feat_importances.nlargest(5).plot(kind='barh')
-plt.title('Feature Importance')
-plt.tight_layout()
-plt.show()
-
-
-# #####################################
-# N  E  U  R  A  L - N  E  T  W  O  R  K 
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.optimizers import Adam
+# feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+# feat_importances.nlargest(5).plot(kind='barh')
+# plt.title('Feature Importance')
+# plt.tight_layout()
+# plt.show()
 
 
+# # #####################################
+# # N  E  U  R  A  L - N  E  T  W  O  R  K 
 
-# having 13 neuron is based on the number of available features
-model = Sequential()
-model.add(Dense(13,activation='relu'))
-model.add(Dense(13,activation='relu'))
-model.add(Dense(13,activation='relu'))
-model.add(Dense(13,activation='relu'))
-model.add(Dense(1))
-model.compile(optimizer='Adam',loss='mse')
-
-model.fit(x=X_train,y=y_train,
-          validation_data=(X_test,y_test),
-          batch_size=128,epochs=400)
-model.summary()
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Activation
+# from tensorflow.keras.optimizers import Adam
 
 
-loss_df = pd.DataFrame(model.history.history)
-loss_df.plot(figsize=(12,8))
 
-y_pred = model.predict(X_test)
-from sklearn import metrics
-print('MAE:', metrics.mean_absolute_error(y_test, y_pred))  
-print('MSE:', metrics.mean_squared_error(y_test, y_pred))  
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-print('VarScore:',metrics.explained_variance_score(y_test,y_pred))
-# Visualizing Our predictions
-fig = plt.figure(figsize=(10,5))
-plt.scatter(y_test,y_pred)
-# Perfect predictions
-plt.plot(y_test,y_test,'r')
-plt.tight_layout()
-plt.show()
+# # having 13 neuron is based on the number of available features
+# model = Sequential()
+# model.add(Dense(13,activation='relu'))
+# model.add(Dense(13,activation='relu'))
+# model.add(Dense(13,activation='relu'))
+# model.add(Dense(13,activation='relu'))
+# model.add(Dense(1))
+# model.compile(optimizer='Adam',loss='mse')
+
+# model.fit(x=X_train,y=y_train,
+#           validation_data=(X_test,y_test),
+#           batch_size=128,epochs=400)
+# model.summary()
+
+
+# loss_df = pd.DataFrame(model.history.history)
+# loss_df.plot(figsize=(12,8))
+
+# y_pred = model.predict(X_test)
+# from sklearn import metrics
+# print('MAE:', metrics.mean_absolute_error(y_test, y_pred))  
+# print('MSE:', metrics.mean_squared_error(y_test, y_pred))  
+# print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# print('VarScore:',metrics.explained_variance_score(y_test,y_pred))
+# # Visualizing Our predictions
+# fig = plt.figure(figsize=(10,5))
+# plt.scatter(y_test,y_pred)
+# # Perfect predictions
+# plt.plot(y_test,y_test,'r')
+# plt.tight_layout()
+# plt.show()
+
 
 
 
@@ -263,17 +259,34 @@ plt.show()
 # plt.tight_layout()
 # plt.show()
 
-# fig = plt.figure(figsize=(16,5))
-# fig.add_subplot(2,2,1)
-# sns.scatterplot(normData['Wave_height'], normData['Viscosity_cst'])
-# fig.add_subplot(2,2,2)
-# sns.scatterplot(normData['F.O_Temp_Celcius'],normData['Viscosity_cst'])
-# fig.add_subplot(2,2,3)
-# sns.scatterplot(normData['L.O.Main_Temp_Celcius'],normData['Viscosity_cst'])
-# fig.add_subplot(2,2,4)
-# sns.scatterplot(normData['L.O.Main_Pressure_kgm3'],normData['Viscosity_cst'])
+
+pt = PowerTransformer(method='yeo-johnson' )
+dataTransform = pt.fit_transform(Xfeat)
+# convert the array back to a dataframe
+datasetYeo = pd.DataFrame(dataTransform, columns=['Load_pct', 'Engine-RPM', 'SOG', 'STW', 'DFT', 'L.O.Main_Pressure_kgm3',
+                                                  'L.O.Main_Temp_Celcius', 'RWS', 'RWD' ,'Wave_height','Air_Temp', 'F.O_pressure','F.O_Temp_Celcius'  ])
+# histograms of the variables
+# plt.hist(datasetYeo)
 # plt.tight_layout()
 # plt.show()
+
+print(datasetYeo.head(6))
+
+
+
+
+
+fig = plt.figure(figsize=(16,5))
+fig.add_subplot(2,2,1)
+sns.kdeplot(x = datasetYeo['Wave_height'], y =  normData['Viscosity_cst'])
+fig.add_subplot(2,2,2)
+sns.kdeplot(x = datasetYeo['Engine-RPM'],y = normData['Viscosity_cst'])
+fig.add_subplot(2,2,3)
+sns.kdeplot(x = datasetYeo['SOG'], y = normData['Viscosity_cst'])
+fig.add_subplot(2,2,4)
+sns.kdeplot(x =datasetYeo['STW'],y = normData['Viscosity_cst'])
+plt.tight_layout()
+plt.show()
 
 # fig = plt.figure(figsize=(15,7))
 # fig.add_subplot(2,2,1)
@@ -286,5 +299,4 @@ plt.show()
 # sns.countplot(normData['L.O.Main_Pressure_kgm3'])
 # plt.tight_layout()
 # plt.show()
-
 
